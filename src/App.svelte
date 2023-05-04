7c381ee7195e2a5179a2229f51c3e72e79e31900
DanielOjeda25 <danielalejandroojeda03@gmail.com>
2023-05-03 22:05:46 -0300

    nuevo formulario creado

--- src/App.svelte
+++ src/App.svelte
@@ -1,20 +1,9 @@
-<script lang="ts">
-
-let tasks = {
-  title : '',
-  description :''
-}
+<script>
+  import Form from "./lib/Form.svelte";
 </script>
 
 <main>
-  <div>Formulario de tarea</div> 
-  <form>
-    <label>Title</label>
-    <input type="text" name="" placeholder="Escribe un titulo">
-
-    <label for="description">Description</label>
-    <textarea id="description" placeholder="Escribe una descripcion" rows="3"></textarea>
-  </form>
+  <Form />
 </main>
 
 <style>
---
+++ src/lib/Form.svelte
@@ -0,0 +1,44 @@
+<script lang="ts">
+  import { doc, addDoc, collection } from "firebase/firestore";
+  import { DB } from "../firebase";
+
+  export let tasks = {
+    title: "",
+    description: "",
+  };
+
+  export async function handleSubmit() {
+    try {
+      await addDoc(collection(DB, "tasks"), tasks);
+      alert("tarea subida");
+    } catch (error) {
+      console.error("No fue posible subir la tarea");
+      alert("Error al subir la tarea");
+    }
+  };
+</script>
+
+<main>
+  <div>Formulario de tarea</div>
+  <form on:submit|preventDefault={handleSubmit}>
+    <label>Title</label>
+    <input
+      bind:value={tasks.title}
+      type="text"
+      name=""
+      placeholder="Escribe un titulo"
+    />
+
+    <label for="description">Description</label>
+    <textarea
+      bind:value={tasks.description}
+      id="description"
+      placeholder="Escribe una descripcion"
+      rows="3"
+    />
+    <button>Guardar Tarea</button>
+  </form>
+</main>
+
+<style>
+</style>
